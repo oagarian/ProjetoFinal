4488333bfacd3e1eee88da8496743e1d0b911b67
oagarian <tns6@aluno.ifal.edu.br>
2022-10-22 10:40:28 -0300

    Envio dos arquivos!

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView,StyleSheet, Text, View, Dimensions , Button} from 'react-native';
+import React from "react";
+import { TextInput } from 'react-native-paper'
+import MapView from 'react-native-maps'
 
-export default function App() {
+
+const MyComponent = (style=styles.container) => {  
+  const [latitude, setLatitude] = React.useState(0);
+  const [longitude, setLongitude] = React.useState(0);
+  function mostrar() {
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <View style={{padding: 10}}>
+    <TextInput
+      label="Latitude"
+      value={latitude}
+      onSubmitEditing={newLatitude => setLatitude(newLatitude)}
+      defaultValue={latitude}
+      keyboardType="numeric"
+    />
+    <TextInput
+      label="Longitude"
+      value={longitude}
+      onSubmitEditing={newLongitude => setLongitude(newLongitude)}
+      defaultValue={longitude}
+      keyboardType="numeric"
+    />
+    
+    <MapView style={styles.map} region={{latitude: latitude, longitude: longitude}} showsUserLocation={true} /> 
+   
+  </View>
+   // TODO: Remover bug e corrigir o envio de dados 
   );
 }
 
+export default MyComponent
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: (Dimensions.get('window').width)-20,
+    height: (Dimensions.get('window').height)-15,
   },
 });
